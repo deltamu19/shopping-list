4002728a19b2541c0f6bdacba5591f72dd5966dd
deltamu19 <daryll.martinez@gmail.com>
2016-06-18 14:15:40 -0700

    js edit 5-added alert if no selection

--- js/app.js
+++ js/app.js
@@ -17,13 +17,17 @@ $(document).ready(function() {
 	//Move items from ready to done list//
 	$('#done-move').click(function() {
 		var checked = $('#ready-form input:checked')
+		
 		for (var i = 0; i < checked.length; i++) {
 			var item = checked[i]
 			var li = $(item).parent().parent()
 			$('.done-container ul').append(li)
 		}
 		document.getElementById('done-form').reset()
-
+		if (checked.length == 0) {
+			alert('Please select at least one item to move.')
+			return false
+		}	
 	})
 
 	//Move items from done to ready list//
@@ -35,6 +39,10 @@ $(document).ready(function() {
 			$('.ready-container ul').append(li)
 		}
 		document.getElementById('ready-form').reset()
+		if (checked.length == 0) {
+			alert('Please select at least one item to move.')
+			return false
+		}
 	})
 
 	//Delete checked items//
@@ -44,6 +52,10 @@ $(document).ready(function() {
 			var item = checked[i]
 			var li = $(item).parent().parent()
 			$(li).remove()
+		}
+		if (checked.length == 0) {
+			alert('Please select at least one item to move.')
+			return false
 		}
 	})
 })
