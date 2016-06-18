00530ec05685b4b4db37f4a4b0c651b428a2abe6
deltamu19 <daryll.martinez@gmail.com>
2016-06-17 20:44:29 -0700

    js edit 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-	$('input[name=new-item]').val().prepend()
+	$('#new-item').submit(function() {
+		var value = $('#new-input').val();	
+		  var html = 
+		  '<li>' + 
+		  	'<div class="checkbox">' +
+				'<input type="checkbox" name="items">'+ value +
+              	'<br>' +
+            '</div>' +
+          '</li>'
+		$(".ready-container ul").append(html)
+		document.getElementById("new-item").reset()
+		return false
+	})
+	$('#done-move').click(function() {
+		var checked = $('input:checked')
+		for (var i = 0; i < checked.length; i++) {
+			var item = checked[i]
+			var li = $(item).parent().parent()
+			$('.done-container ul').append(li)
+		}
+		document.getElementById('list-form').reset()
+	})
+
 });
+
+li.css("display","none")
